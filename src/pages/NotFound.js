3e2beaca8527ae58fcd5b5e2dff19ab3f8106c09
Dinh Hai <dinhhhaii@gmail.com>
2020-03-26 15:00:01 +0700

    Update Notfound Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const NotFound = () => {
-  return (<div>Test </div>);
+  return (
+    <main>
+      <div id="error_page">
+        <div className="container">
+          <div className="row justify-content-center text-center">
+            <div className="col-xl-7 col-lg-9">
+              <h2>404 <i className="icon_error-triangle_alt" /></h2>
+              <p>We're sorry, but the page you were looking for doesn&apos;t exist.</p>
+              <form>
+                <div className="search_bar_error">
+                  <input type="text" className="form-control" placeholder="What are you looking for?" />
+                  <input type="submit" value="Search" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default NotFound;
